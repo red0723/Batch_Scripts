1d78463b1069497fca4aac5dd790221b959f994c
red0723 <red0723@frontier.com>
2024-12-17 11:31:27 -0500

    Add files via upload
    Updated path to be set into a variable

--- Extract_Zipped_Files.bat
+++ Extract_Zipped_Files.bat
@@ -1,24 +1,22 @@
 @echo off
 :: Change to the directory where the batch file is located
 cd /d %~dp0
+set "MY_EXTRACTEDPATH=C:\Extracted_Files"
 
-:: Check if the destination directory exists, if not, create it
-if not exist "C:\Extracted_Files\" (
-    mkdir "C:\Extracted_Files"
+:: Check if the destination directory exists, if 0not, create it
+if not exist "%MY_EXTRACTEDPATH%" (
+    mkdir "%MY_EXTRACTEDPATH%"
 )
 
 :: Extract all zip files to the C:\ directory
 for %%f in (*.zip) do (
-    powershell -Command "Expand-Archive -Path '%%f' -DestinationPath 'C:\Extracted_Files'"
+    powershell -Command "Expand-Archive -Path '%%f' -DestinationPath '%MY_EXTRACTEDPATH%'"
     powershell -Command "del '%%f'"
 )
 
-:: Delete all files and subdirectories in the current directory except the batch file
-
-
 :: Display a message for 3 seconds
-echo Extraction completed. The files have been extracted to C:\Extracted_Files.
-timeout /t 10 /nobreak > nul
+echo Extraction completed. The files have been extracted to %MY_EXTRACTEDPATH%.
+timeout /t 5 /nobreak > nul
 
 :: Optional: Clear the screen after the timeout
 cls
